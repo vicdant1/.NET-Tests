563aaca9275150a9394389bd9794b3b7e74b75b4
vicdant1 <dantjv@outlook.com>
2022-08-28 14:38:02 -0300

    adding using statement + dispose method information

--- ConsoleYieldApp/Program.cs
+++ ConsoleYieldApp/Program.cs
@@ -3,12 +3,53 @@ using ConsoleYieldApp.Entities;
 
 Console.WriteLine("Program started");
 
-IEnumerable<Person> People = PeopleHelper.GetPeople();
 
-foreach (var person in People)
-{
-    if (int.Parse(person.Cpf) == 5)
-        break;
+#region yield_return
+//IEnumerable<Person> People = PeopleHelper.GetPeople();
 
-    Console.WriteLine($"Id: {person.Id} || CPF: {person.Cpf}");
-}
+//foreach (var person in People)
+//{
+//    if (int.Parse(person.Cpf) == 5)
+//        break;
+
+//    Console.WriteLine($"Id: {person.Id} || CPF: {person.Cpf}");
+//}
+#endregion
+
+#region using_statement
+
+/*
+ using statement will promote a behave like this piece of code:
+    
+    try
+    {
+        instance.something();
+    }
+    finally
+    {
+        instance.Dispose();
+    }
+
+    It SHOULD be used when we are dealing with classes that implements IDisposable interface.
+    This way we are avoiding memory leaks through the program because .NET garbage collector will take a deeper look at it.
+
+    example:
+
+    using(var connection = new SqlConnection())
+    {
+        connection.Open();
+        Command dbCommand = connection.CreateCommand();
+
+        connection.ExecuteCommand(dbCommand);
+
+
+        using(var dataReader = new System.DataReader())
+        {
+            dataReader.ReadData(connection);
+        }
+    }
+
+ */
+
+
+#endregion
